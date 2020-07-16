720c2e356e58cfb3b38f6d8368bbc79e937a7e90
Hideki Sekine <sekineh@me.com>
2020-07-16 09:30:51 +0900

    Ignore CRLF vs LF diffrense in bash testing.

--- test.sh
+++ test.sh
@@ -6,19 +6,19 @@ error_exit() {
 OUTFILE=tmpfilename
 
 python ffpt2csv.py test/1.ffpt $OUTFILE || error_exit
-diff -u test/1.csv $OUTFILE || error_exit
+diff -u --ignore-space-change test/1.csv $OUTFILE || error_exit
 rm $OUTFILE
 
 python csv2ffpt.py test/1.csv $OUTFILE || error_exit
-diff -u test/1.ffpt $OUTFILE || error_exit
+diff -u --ignore-space-change test/1.ffpt $OUTFILE || error_exit
 rm $OUTFILE
 
 python ffpt2csv.py test/2.ffpt $OUTFILE || error_exit
-diff -u test/2.csv $OUTFILE || error_exit
+diff -u --ignore-space-change test/2.csv $OUTFILE || error_exit
 rm $OUTFILE
 
 python csv2ffpt.py test/2.csv $OUTFILE || error_exit
-diff -u test/2.ffpt $OUTFILE || error_exit
+diff -u --ignore-space-change test/2.ffpt $OUTFILE || error_exit
 rm $OUTFILE
 
 
