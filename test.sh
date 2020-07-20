28d74932e352e7f10492a51e8ac2679f551f5dc4
Hideki Sekine <sekineh@me.com>
2020-07-20 17:33:41 +0900

    fix bash test

--- test.sh
+++ test.sh
@@ -5,19 +5,19 @@ error_exit() {
 
 OUTFILE=tmpfilename
 
-python ffpt2csv.py tests/data/1.ffpt $OUTFILE || error_exit
+python ffptutils/scripts/ffpt2csv.py tests/data/1.ffpt $OUTFILE || error_exit
 diff -u --ignore-space-change tests/data/1.csv $OUTFILE || error_exit
 rm $OUTFILE
 
-python csv2ffpt.py tests/data/1.csv $OUTFILE || error_exit
+python ffptutils/scripts/csv2ffpt.py tests/data/1.csv $OUTFILE || error_exit
 diff -u --ignore-space-change tests/data/1.ffpt $OUTFILE || error_exit
 rm $OUTFILE
 
-python ffpt2csv.py tests/data/2.ffpt $OUTFILE || error_exit
+python ffptutils/scripts/ffpt2csv.py tests/data/2.ffpt $OUTFILE || error_exit
 diff -u --ignore-space-change tests/data/2.csv $OUTFILE || error_exit
 rm $OUTFILE
 
-python csv2ffpt.py tests/data/2.csv $OUTFILE || error_exit
+python ffptutils/scripts/csv2ffpt.py tests/data/2.csv $OUTFILE || error_exit
 diff -u --ignore-space-change tests/data/2.ffpt $OUTFILE || error_exit
 rm $OUTFILE
 
