b1e1c2b5b65cfef91bc45e7441e380d37e8a1ba5
Hideki Sekine <sekineh@me.com>
2020-07-20 17:02:06 +0900

    Move entry points

--- setup.py
+++ setup.py
@@ -9,14 +9,29 @@ setuptools.setup(
     author='Hideki Sekine',
     author_email='sekineh@me.com',
     description='a python library that reads and writes Spirent iTest ParameterTree (.ffpt) files',
+    license='MIT',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sekineh/ffptutils-py",
     packages=setuptools.find_packages(),
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Intended Audience :: Telecommunications Industry",
+        "Topic :: Utilities",
     ],
     python_requires='>=3.6',
+    entry_points={  # Optional
+        'console_scripts': [
+            'ffpt2csv=ffptutils.ffpt2csv:main',
+            'csv2ffpt=ffptutils.csv2ffpt:main',
+        ],
+    },
+    project_urls={  # Optional
+        'Bug Reports': 'https://github.com/sekineh/ffptutils-py/issues',
+        # 'Funding': 'https://donate.pypi.org',
+        # 'Say Thanks!': 'http://saythanks.io/to/example',
+        'Source': 'https://github.com/sekineh/ffptutils-py/',
+    },
 )
