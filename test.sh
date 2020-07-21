338d9a95ef6110cee304e90046a93c21a7bf7e89
Hideki Sekine <sekineh@me.com>
2020-07-21 11:17:32 +0900

    CI: add package installation step

--- test.sh
+++ test.sh
@@ -5,19 +5,19 @@ error_exit() {
 
 OUTFILE=tmpfilename
 
-python ffptutils/scripts/ffpt2csv.py tests/data/1.ffpt $OUTFILE || error_exit
+ffpt2csv tests/data/1.ffpt $OUTFILE || error_exit
 diff -u --ignore-space-change tests/data/1.csv $OUTFILE || error_exit
 rm $OUTFILE
 
-python ffptutils/scripts/csv2ffpt.py tests/data/1.csv $OUTFILE || error_exit
+csv2ffpt tests/data/1.csv $OUTFILE || error_exit
 diff -u --ignore-space-change tests/data/1.ffpt $OUTFILE || error_exit
 rm $OUTFILE
 
-python ffptutils/scripts/ffpt2csv.py tests/data/2.ffpt $OUTFILE || error_exit
+ffpt2csv tests/data/2.ffpt $OUTFILE || error_exit
 diff -u --ignore-space-change tests/data/2.csv $OUTFILE || error_exit
 rm $OUTFILE
 
-python ffptutils/scripts/csv2ffpt.py tests/data/2.csv $OUTFILE || error_exit
+csv2ffpt tests/data/2.csv $OUTFILE || error_exit
 diff -u --ignore-space-change tests/data/2.ffpt $OUTFILE || error_exit
 rm $OUTFILE
 
